12f25a3c47c84e5116c00492fbeeb10149edd1e6
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-06-25 20:13:54 +0530

    Post List for feed

---
+++ client/src/scenes/widgets/AllPosts.jsx
@@ -0,0 +1,88 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPost, setPosts } from "../../state/slice";
+import Post from "./Post";
+
+function AllPosts({ userId, isProfile }) {
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
+  const posts = useSelector((state) => state.posts);
+  const token = useSelector((state) => state.token);
+
+  async function getPosts() {
+    try {
+      console.log("GETTING POSTS FOR FEED")
+      setLoading(true);
+      const response = await fetch("http://localhost:5000/posts", {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      const data = await response.json();
+      dispatch(setPosts({ posts: data }));
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
+  }
+
+  async function getUserPosts() {
+    try {
+      console.log("GETTING USER POST")
+      setLoading(true);
+      const response = await fetch(`http://localhost:5000/posts/${userId}/posts`, {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      const data = await response.json();
+      console.log("ALL POST DATA: ", data);
+      dispatch(setPost({ posts: data }));
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    isProfile ? getUserPosts() : getPosts();
+  }, [userId, token, isProfile]);
+
+  if (loading) {
+    return <p className="text-center text-gray-500">Loading posts...</p>;
+  }
+
+  return (
+    <div className="space-y-6">
+      {posts.map(
+        ({
+          _id,
+          userId,
+          firstName,
+          lastName,
+          description,
+          location,
+          picturePath,
+          userPicturePath,
+          likes,
+          comments,
+        }) => (
+          <Post
+            key={_id}
+            postId={_id}
+            postUserId={userId}
+            name={`${firstName} ${lastName}`}
+            description={description}
+            location={location}
+            picturePath={picturePath}
+            userPicturePath={userPicturePath}
+            likes={likes}
+            comments={comments}
+          />
+        )
+      )}
+    </div>
+  );
+}
+
+export default AllPosts;
