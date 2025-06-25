7b4f28fba9e385a8fc9b447bfff0ed4202ba7fc8
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-06-25 20:15:32 +0530

    Redux state manager and store

---
+++ client/src/state/slice.js
@@ -0,0 +1,54 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  mode: "light",
+  user: null,
+  token: null,
+  posts: [],
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setMode: (state) => {
+      state.mode = state.mode === "light" ? "dark" : "light";
+    },
+    setLogin: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+    setLogout: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+    setFriend: (state, action) => {
+      if (state.user) {
+        state.user.friends = action.payload.friends;
+      } else {
+        console.error("Cant add as friends to non existing user.");
+      }
+    },
+    setPosts: (state, action) => {
+      state.posts = action.payload.posts;
+    },
+    setPost: (state, action) => {
+      /*const incomingPosts = action.payload.posts;
+      const updatedPosts = state.posts.map((post) => {
+        const matchedPost = incomingPosts.find(
+          (newPost) => newPost._id === post._id
+        );
+        return matchedPost ? matchedPost : post;
+      });
+      state.posts = updatedPosts;*/
+      state.posts = action.payload.posts;
+    },
+  },
+});
+
+const authReducer = authSlice.reducer;
+
+export const { setMode, setLogin, setLogout, setFriend, setPosts, setPost } =
+  authSlice.actions;
+
+export default authReducer;
---
+++ client/src/state/store.js
@@ -0,0 +1,29 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./slice.js";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = { key: "root", storage, version: 1 };
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const storePersistor = persistStore(store);
